c78825d416bde3fa2601a6b38862e15b1af685db
slie <sliewdyinwhite@gmail.com>
2021-04-13 20:38:40 +0800

    采用API形式获取url

--- src/plugins/rand_bqb/data_source.py
+++ src/plugins/rand_bqb/data_source.py
@@ -1,11 +1,14 @@
 import httpx
-import json
+
 async def get_image(key:str):
     '''
     根据关键词在图床中提取image的url
     '''
-    with open('rand_bqb/BQkeys.json','r',encoding='utf8')as fp:
-        json_data = json.load(fp)
+    # 调取表情包链接api
+    async with httpx.AsyncClient() as client:
+        resp = await client.get('https://my-json-server.typicode.com/sliefamily/bdg-bot/db')
+        logger.debug(resp.json())
+        json_data = resp.json()['detail']
     for i in range(0,len(json_data)):
         if json_data[i]["key"] == key:
             await json_data[i]["url"]
