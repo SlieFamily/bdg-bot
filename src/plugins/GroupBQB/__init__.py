eda2ebc67e83c527f58d0fcdd7dbe56254701cca
slie <sliewdyinwhite@gmail.com>
2021-04-17 22:53:45 +0800

    修改群表情包逻辑以及包名“ '

---
+++ src/plugins/GroupBQB/__init__.py
@@ -0,0 +1,26 @@
+import nonebot
+from .config import Config
+from nonebot import on_command
+from nonebot.rule import to_me
+from nonebot.typing import T_State
+from nonebot.adapters import Bot, Event
+from nonebot.adapters.cqhttp import Message,MessageSegment
+
+from .data_source import get_words
+
+# 默认配置
+global_config = nonebot.get_driver().config
+plugin_config = Config(**global_config.dict())
+
+# 响应命令
+BDwords = on_command("BDwords", aliases=set(['*/爆点语录','*/BD语录','*/bd语录']), priority=2)
+
+@BDwords.handle()
+async def handle_first_receive(bot: Bot, event: Event, state: T_State):
+    words = await get_words()
+    await BDwords.finish(Message(words))
+
+
+
+
+
