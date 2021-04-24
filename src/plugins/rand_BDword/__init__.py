4b6ad78ff61c6f3821f8aa12a8a3ec32a572d7bc
slie <sliewdyinwhite@gmail.com>
2021-04-24 11:48:37 +0800

    添加自助删除上一条语录的功能

--- src/plugins/rand_BDword/__init__.py
+++ src/plugins/rand_BDword/__init__.py
@@ -7,7 +7,7 @@ from nonebot.typing import T_State
 from nonebot.adapters import Bot, Event
 from nonebot.adapters.cqhttp import Message,MessageSegment
 
-from .data_source import get_words,IsAdded
+from .data_source import get_words,IsAdded,IsDel
 
 # 默认配置
 global_config = nonebot.get_driver().config
@@ -15,7 +15,8 @@ plugin_config = Config(**global_config.dict())
 
 # 响应命令
 BDwords = on_command("BDwords", aliases=set(['*/爆点语录','*/BD语录','*/bd语录']), priority=2)
-Addwords = on_regex("\*/add ([\s\S]+) \*/end")
+Addwords = on_regex("\*/add\n([\s\S]+)\n\*/end")
+Delwords = on_command("*/del",priority=2)
 
 @BDwords.handle()
 async def handle_first_receive(bot: Bot, event: Event, state: T_State):
@@ -25,10 +26,15 @@ async def handle_first_receive(bot: Bot, event: Event, state: T_State):
 
 @Addwords.handle()
 async def handle_first_receive(bot: Bot,event: Event, state: T_State):
-    words = re.findall("\*/add ([\s\S]+) \*/end",str(event.get_message()))
+    words = re.findall("\*/add\n([\s\S]+)\n\*/end",str(event.get_message()))
     if IsAdded(words):
-        await Addwords.finish(Message("语录追加成功！爆点世界重构中~"))
+        await Addwords.finish(Message("语录追加成功，爆点世界重构中……"))
     else:
-        await Addwords.finish(Message("语录追加失败了，吖屎啦！"))
+        await Addwords.finish(Message("追加失败了，吖屎啦！"))
 
-
+@Delwords.handle()
+async def handle_first_receive(bot: Bot,event: Event, state: T_State):
+    if IsDel():
+        await Addwords.finish(Message("语录删除成功，哭唧唧……"))
+    else:
+        await Addwords.finish(Message("删除失败了，嗯嘿嘿"))
--- src/plugins/rand_BDword/data_source.py
+++ src/plugins/rand_BDword/data_source.py
@@ -39,3 +39,15 @@ def IsAdded(words):
         print
         return True
     return False
+
+def IsDel():
+    '''
+    删除上一条添加的语句
+    '''
+    json_data = get_json()
+    json_data['BDwords'].pop()
+    with open("/home/bdg-bot/db.json", "w", encoding="utf8") as fp:
+        json.dump(json_data, fp,ensure_ascii=False)
+        print
+        return True
+    return False
